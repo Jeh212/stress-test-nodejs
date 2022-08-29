a4fc8752b229228a7a61443aa6fdb301948b86c3
Jean carlos <jeandub1@hotmail.com>
2022-08-29 14:04:56 -0400

    commit

---
+++ simple-apu/src/server.js
@@ -0,0 +1,20 @@
+import { Console } from "console";
+import http from "http";
+const processId = process.pid;
+
+const server = http.createServer((request, response) => {
+  for (let index = 0; index < 1e7; index++) {
+    response.end(`handle the pid ${processId} `);
+  }
+});
+
+server.listen(3000).once("listening", () => {
+  console.log("Server started in process", processId);
+});
+
+//Aguardar as conexoes sem encerradas para só então encerrar o programa
+
+process.on("SIGTERM", () => {
+  console.log("server ending", new Date().toISOString());
+  server.close(() => process.exit());
+});
